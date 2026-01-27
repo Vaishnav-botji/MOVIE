cbdf3f97d8f63af579bf22fef57ae81389ea9174
Vaishnav-botji <firestatus899@gmail.com>
2026-01-27 11:40:09 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -229,7 +229,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/+b6pZNRtWIjI5ZmY1">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Vaishanv">[ वैष्णव  ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
